40ff7e89ba40ca70106fcd03d885b2912ce8b53b
mihirogi <mihirogi0000@gmail.com>
2019-04-04 00:35:43 +0900

    HOT

--- ts/sample7.ts
+++ ts/sample7.ts
@@ -1,5 +1,5 @@
-import {from, interval, of} from 'rxjs';
-import {map, take, tap} from 'rxjs/operators';
+import {ConnectableObservable, interval, of, Subject} from 'rxjs';
+import {map, publish, refCount, take, tap} from 'rxjs/operators';
 
 // subscribeしないと値が流れないパターン : COLD
 
@@ -37,8 +37,69 @@ const sample7_3 = () => {
 };
 
 
+// subscribeしなくても値が流れるパターン : HOT
+// connect : ConnectableObservableの値を、Subscribeされていなくても、ストリームに流す
+const sample7_4 = () => {
+    const clock$ = interval(1000)
+        .pipe(
+            take(10),
+            tap(x => {
+                console.log(`do : ${x}`);
+            }),
+            publish()
+        ) as ConnectableObservable<number>;
+
+    clock$.connect();
+
+    setTimeout(() => {
+        clock$.subscribe(x => console.log(`         B : ${x}`))
+    }, 2500);
+};
+
+// ストリームの分岐
+const sample7_5 = () => {
+    const clock$ = interval(1000)
+        .pipe(
+            take(10),
+            tap(x => {
+                console.log(`do : ${x}`);
+            }),
+            publish()
+        ) as ConnectableObservable<number>;
+
+    clock$.connect();
+
+    setTimeout(() => {
+        clock$
+            .pipe(map(x => x * 10))
+            .subscribe(x => console.log(`         B : ${x}`))
+    }, 2500);
+};
+
+// refCount : Observer(Subscribeしてる)が1つ以上あるときはConnectを維持して、0になるとConnectを解除する
+// 最初にSubscribeされた瞬間に、ストリームに値が流れ出す
+const sample7_6 = () => {
+    const clock$ = interval(1000)
+        .pipe(
+            take(10),
+            tap(x => {
+                console.log(`do : ${x}`);
+            }),
+            publish(),
+            refCount()
+        ) as ConnectableObservable<number>;
+
+    clock$.pipe(
+        map(x => x * 10)
+    ).subscribe(x => console.log(`         B : ${x}`));
+};
+
+
 export {
     sample7_1,
     sample7_2,
-    sample7_3
+    sample7_3,
+    sample7_4,
+    sample7_5,
+    sample7_6
 }
